f9e95bef0b2aaf59df6f1bd0edb854c0ced3edd3
BleuHund <dtcannisi@gmail.com>
2023-04-11 12:32:10 -0400

    Update setup.py
    Updating version for pull request.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='toluene',
-    version='0.0.0',
+    version='0.0.9',
     description='Python library for geospatial and image processing functions',
     packages=[
         'toluene.base',
