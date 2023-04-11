22831e61afe6468a894a52b5eb1e50d23115fc76
DTFuchs <dtcannisi@gmail.com>
2023-04-11 15:18:20 -0400

    Added license and other good information to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,29 @@
 from setuptools import setup
 
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
 setup(
     name='toluene',
     version='0.0.9',
+    liscense='MIT',
     description='Python library for geospatial and image processing functions',
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft',
+        'Operating System :: POSIX',
+        'Programming Language :: C',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Scientific/Engineering :: GIS',
+        'Topic :: Scientific/Engineering :: Image Processing',
+        'Typing :: Typed'
+    ],
     packages=[
         'toluene',
     ],
