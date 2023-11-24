4e3f89c1ce4cc733e333cf4c343640389a607d8f
BleuHund <dtcannisi@gmail.com>
2023-11-24 12:56:04 -0500

    Started adding polar motion to my equation.

--- test/test_script.py
+++ test/test_script.py
@@ -7,4 +7,7 @@ from toluene.core.time import TerrestrialTimeJ2000
 TT = TerrestrialTimeJ2000(datetime(2023, 11, 20, 0, 0, 0, tzinfo=timezone.utc))
 
 ecef = ECEF(50000, 50000, 50000, time=TT)
-print(ecef.magnitude(), ecef.to_eci().magnitude())
+eci = ecef.to_eci()
+print(ecef.x, ecef.y, ecef.z)
+print(eci.x, eci.y, eci.z)
+print(ecef.magnitude(), eci.magnitude())
